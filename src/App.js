683467be1408e5c64b93df2fb5ef6002b3f3d89c
takahiro38k <grandeur38k@gmail.com>
2020-04-11 16:50:46 +0900

    implement callbacks with useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const App = (props) => {
   // const [name, setName] = useState(props.name)
@@ -12,7 +12,30 @@ const App = (props) => {
    */
   const [state, setState] = useState(props)
   const { name, price } = state
+
   /**
+   * useEffect(() => {})
+   * レンダリングの後で実行される。また、再レンダリングの後にも実行される。
+   */
+  useEffect(() => {
+    console.log('This is like componentDidMount or componentDidUpdate.')
+  })
+  /**
+   * useEffect(() => {}, [])
+   * レンダリングの後にだけ実行される。
+   */
+  useEffect(() => {
+    console.log('This is like componentDidMount only.')
+  }, [])
+  /**
+   * useEffect(() => {}, [state])
+   * 指定した状態(下記の場合name)がレンダリングおよび再レンダリングされた時に実行される。
+   */
+  useEffect(() => {
+    console.log('This callback is for "name" only.')
+  }, [name])
+
+   /**
    * コードが短いので、JSXの中に直接記載。
    */
   // const reset = () => {
@@ -63,6 +86,73 @@ App.defaultProps = {
   name: '',
   price: 2000,
 }
+
+// ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
+// ▼implement object state▼
+
+// const App = (props) => {
+//   // const [name, setName] = useState(props.name)
+//   // const [price, setPrice] = useState(props.price)
+//   /**
+//    * defaultpropsを上記のように個別に受け取るのではなく、
+//    * まとめてオブジェクトとしてuseState()のparaに受け取ることもできる。
+//    * ◆◆◆◆◆重要◆◆◆◆◆
+//    * propsがオブジェクトなので、当然stateもオブジェクトになる。
+//    * そうなると、setStateもオブジェクトを返さなければならない(スプレッド演算子を活用)。
+//    */
+//   const [state, setState] = useState(props)
+//   const { name, price } = state
+//   /**
+//    * コードが短いので、JSXの中に直接記載。
+//    */
+//   // const reset = () => {
+//   //   // setPrice(props.price)
+//   //   // setName(props.name)]
+//   //   setState(props)
+//   // }
+
+//   return (
+//     <>
+//       <p>現在の「{name}」は、{price}円です。</p>
+//       {/*
+//       setxxxx()
+//       # paraに値・関数どちらでも設定できる。
+//       # paraが関数の場合、その関数のparaに現在の状態を受け取ることができる。
+//         返り値は必ず状態を返すようにすること。
+//       # setxxxx()によって状態が変化すると、変更箇所が再renderされる。
+//        */}
+//       {/*
+//       setStateのpara
+//       スプレッド演算子を使うことで、オブジェクトを展開して渡すことができる。
+//       カンマで区切り、変更部分のみを指定する。
+//        */}
+//       <button onClick={() => setState({...state, price: price + 1})}>+1</button>
+//       <button onClick={() => setState({...state, price: price - 1})}>-1</button>
+//       <button onClick={() => setState(props)}>Reset</button>
+//       {/*
+//       イベントハンドラはparaにeventオブジェクトを受け取ることができる。
+//       引数.target でイベントの起きた要素を取得できる。
+//        */}
+//       {/*
+//       <input value={name} onChange={e => setName(e.target.value)} />
+//        */}
+//       {/*
+//       setStateのpara
+//       スプレッド演算子を使うことで、オブジェクトを展開して渡すことができる。
+//       カンマで区切り、変更部分のみを指定する。
+//        */}
+//       <input
+//         value={name}
+//         onChange={e => setState({...state, name: e.target.value})}
+//       />
+//     </>
+//   )
+// }
+
+// App.defaultProps = {
+//   name: '',
+//   price: 2000,
+// }
 
 
 // ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
