c95911628ad3d16ad9317fb533533e1806e2c38e
takahiro38k <grandeur38k@gmail.com>
2020-04-12 21:42:24 +0900

    implement event creation

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useReducer, useState } from 'react'
 // jQueryに依存することなく、最低限styleを適用できるようにする。
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import reducer from '../reducers'
+
 const App = () => {
+  /*
+  useReducer(reducer, initialArg[, init])
+  ++++----------------
+  1st para   reducer
+  2nd para   stateの初期値
+  3rd para   stateの初期化時に発火するcallback
+  reVal      [state, dispatch]
+  ----------------++++
+   */
+  const [state, dispatch] = useReducer(reducer, [])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  const addEvent = e => {
+    /**
+     * preventDefault()
+     * ページの再読み込みを防止する。SPAでは重要な処理。
+     */
+    e.preventDefault()
+
+    // console.log({title, body}) // titleとbodyを取得できているか確認。
+    dispatch({
+      type: 'CREATE_EVENT',
+      title,
+      body,
+    })
+
+    /**
+     * 「イベントを作成する」ボタンをクリックしたらフォームの値をクリア。
+     */
+    setTitle('')
+    setBody('')
+  }
+
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
@@ -11,15 +47,15 @@ const App = () => {
           <label htmlFor="formEventTitle">タイトル</label>
           {/* labelタグのfor(htmlFor)とinputタグのidが一致することで、
             labelにフォーカスが当たるとinputのフォームが選択されるようになる。 */}
-          <input className="form-control" id="formEventTitle"/>
+          <input className="form-control" id="formEventTitle" value={title} onChange={e => setTitle(e.target.value)}/>
         </div>
 
         <div className="form-group">
           <label htmlFor="formEventBody">ボディー</label>
-          <textarea className="form-control" id="formEventBody"/>
+          <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)}/>
         </div>
 
-        <button className="btn btn-primary">イベントを作成する</button>
+        <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
         <button className="btn btn-danger">全てのイベントを削除する</button>
       </form>
 
