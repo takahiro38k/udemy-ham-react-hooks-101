5954427d097ae3bc1df47cfd8c214447f890ed26
takahiro38k <grandeur38k@gmail.com>
2020-04-15 18:52:41 +0900

    display all operation logs

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import EventForm from './EventForm'
 import Events from './Events'
+import OperationLogs from './OperationLogs'
 import AppContext from '../contexts/AppContext'
 /**
  * eventsという名前で実装したが、
@@ -50,6 +51,7 @@ const App = () => {
          */}
         <EventForm />
         <Events />
+        <OperationLogs />
       </div>
     </AppContext.Provider>
   )
