bee6934afe4599e560c04cc603b8060c6a9a8b63
takahiro38k <grandeur38k@gmail.com>
2020-04-15 17:55:41 +0900

    create operation log in case of event deletion

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,7 +1,11 @@
 import React, { useContext } from 'react'
 
-import { DELETE_EVENT } from '../actions'
+import {
+  ADD_OPERATION_LOG,
+  DELETE_EVENT,
+ } from '../actions'
 import AppContext from '../contexts/AppContext'
+import { timeCurrentIso8601 } from '../utils'
 
 // 関数コンポーネントでは、propsを引数として受け取ることができる。
 // const Event = ({ dispatch, event }) => {
@@ -13,7 +17,14 @@ const Event = ({ event }) => {
   const id = event.id
   const handleClickDeleteButton = () => {
     const result = window.confirm(`イベント(id=${id})を本当に削除してもよろしいですか？`)
-    if (result) dispatch({ type: DELETE_EVENT, id })
+    if (result) {
+      dispatch({ type: DELETE_EVENT, id })
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: `イベント(id=${id})を削除しました。`,
+        operatedAt: timeCurrentIso8601(),
+      })
+    }
   }
 
   return (
